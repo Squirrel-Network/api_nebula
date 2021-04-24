1f1bd708fd89f38cd0e94d9a192e883b595d1d94
Marco De Nittis <marco.denittis@gmail.com>
2021-04-24 23:29:27 +0200

    removed previous users, add openapi reference for 401

--- core/api/test.py
+++ core/api/test.py
@@ -2,16 +2,13 @@ from flask import Blueprint
 from flasgger import swag_from
 from core.utilities.auth_manager import auth
 from core.utilities.limiter import limiter
-import os
 
 api_test = Blueprint('api_test', __name__)
-
-print('pwd: %s', os.getcwd())
 
 @api_test.route('/hi', methods=['GET'])
 @limiter.limit("1000 per day")
 @limiter.limit("3/seconds")
 @swag_from('../../openapi/hi.yaml')
-@auth.auth_required()
+#@auth.auth_required()
 def hi():
     return { "status": "hi!" }
--- core/api/users.py
+++ core/api/users.py
@@ -12,62 +12,6 @@ api_users = Blueprint('api_users', __name__)
 @limiter.limit("10/seconds")
 @auth.auth_required()
 @swag_from('../../openapi/users_list.yaml')
-def users():
-    username = request.args.get('username',type=str)
-    order_by = request.args.get('orderby',type=int)
-    start= request.args.get('start', 1, type=int)
-    limit= request.args.get('limit', 20, type=int)
-    data_start = (start,limit)
-    rows = UserRepository().getAll(data_start)
-    countuser = UserRepository().getCountUsers()
-    total = countuser['counter']
-    data = list(map(lambda row: {
-            'id': row['id'],
-            'tg_id': row['tg_id'],
-            'username': row['tg_username'],
-            'created_at': row['created_at'],
-            'updated_at': row['updated_at'],
-        }, rows))
-
-    if username is not None and username != "":
-        row = UserRepository().getByUsername(username)
-        if row:
-            return {'id': row['id'],
-            'tg_id': row['tg_id'],
-            'username': row['tg_username'],
-            'created_at': row['created_at'],
-            'updated_at': row['updated_at']}
-        else:
-            return {'error': 'You have entered an username that does not exist or you have entered incorrect data'}
-
-    if order_by == 1:
-        print("ASC")
-        return {'response': 'ASC'}
-    elif order_by == 2:
-        print("DESC")
-        return {'response': 'DESC'}
-    elif order_by == 3:
-        print("Order by Username")
-        return {'response': 'Order by Username'}
-    elif order_by == 4:
-        print("Order by tgid")
-        return {'response': 'Order by tgid'}
-    else:
-        return jsonify(get_paginated_list(
-        data,
-        '',
-        start,
-        limit,
-        total
-        ))
-
-
-
-@api_users.route('/users2', methods=['GET'])
-@limiter.limit("5000 per day")
-@limiter.limit("10/seconds")
-@auth.auth_required()
-@swag_from('../../openapi/users_list2.yaml')
 def users2():
     params = get_pagination_headers()
     params['tg_username'] = request.args.get('tg_username', None, type=str)
