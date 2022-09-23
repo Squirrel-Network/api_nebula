6c443ba2a4335609b568c0fe6120cbb98387e75a
BluLupo <hersel1991@gmail.com>
2022-09-23 21:02:45 +0200

    fix /news

--- core/api/article.py
+++ core/api/article.py
@@ -4,6 +4,7 @@
 # Copyright SquirrelNetwork
 from flask import Blueprint, request, jsonify
 from core.utilities.limiter import limiter
+from core.utilities.functions import format_iso_date
 from core.database.repository.groups import GroupRepository
 
 api_article = Blueprint('api_article', __name__)
@@ -16,7 +17,9 @@ def article():
     return jsonify(list(map(lambda row: {
         'article_id': row['article_id'],
         'language': row['language'],
-        'content': row['content']
+        'content': row['content'],
+        'created_at': format_iso_date(row['created_at']),
+        'updated_at': format_iso_date(row['updated_at'])
     }, articles)))
 
 
