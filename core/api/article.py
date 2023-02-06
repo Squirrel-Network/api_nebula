6729d2b08978edeb7412268e42e13e40d0f81c6a
BluLupo <hersel1991@gmail.com>
2023-02-06 22:38:18 +0100

    update article endpoint

--- core/api/article.py
+++ core/api/article.py
@@ -10,13 +10,14 @@ from core.database.repository.groups import GroupRepository
 api_article = Blueprint('api_article', __name__)
 
 @api_article.route('/news', methods=['GET'])
-@limiter.limit("2000 per day")
-@limiter.limit("3/seconds")
+@limiter.limit("3000 per day")
+@limiter.limit("5/seconds")
 def article():
     articles = GroupRepository().get_article()
     return jsonify(list(map(lambda row: {
         'article_id': row['article_id'],
-        'title': row['title'],
+        'title': row['article_title'],
+        'category': row['title'],
         'language': row['language'],
         'content': row['content'],
         'created_at': format_iso_date(row['created_at']),
