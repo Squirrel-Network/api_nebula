c5a7a9e642a608446248dd85fc5955fbc6bb0dbd
ByteCntrl <bytecntrl@gmail.com>
2023-07-27 17:04:35 +0200

    Update main

--- main.py
+++ main.py
@@ -6,9 +6,9 @@
 from datetime import datetime
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Form, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from config import Config, Session
 from core.database import init_db
@@ -37,14 +37,14 @@ init_db(app)
 
 
 # Home
-@app.get("/")
+@app.get("/", include_in_schema=False)
 async def index(request: Request):
     data = SuperbanTable.all()
     last_super_banned = await data.order_by("-user_date").limit(8).values()
     count_super_banned = await data.count()
 
     for row in last_super_banned:
-        row["upper"] = row["user_first_name"][:1].upper()
+        row["upper"] = row["user_first_name"][0].upper()
         row["user_time"] = get_formatted_time(str(row["user_date"]))
 
     time_in_utc = datetime.utcnow()
@@ -62,22 +62,34 @@ async def index(request: Request):
 
 
 # User Search
-@app.route("/users", methods=["GET", "POST"])
-@app.route("/users/<username>", methods=["GET"])
-def users_search(username: str = None):
-    if request.method == "POST":
-        username = request.form.get("username").strip().lower()
+@app.get("/users", include_in_schema=False)
+@app.get("/users/{username}", include_in_schema=False)
+async def users_search(request: Request, username: str = None):
+    counter = await Users.all().count()
+    data = await Users.get_or_none(tg_username=username).values()
 
-    with UserRepository() as db:
-        counter = db.get_count_users()
-        data = db.get_by_username(username)
+    return templates.TemplateResponse(
+        "users.html", {"request": request, "data": data, "counter": counter}
+    )
 
-    return render_template("users.html", data=data, counter=counter["counter"])
+
+@app.post("/users", include_in_schema=False)
+async def users_search_post(request: Request, username: str = Form()):
+    counter = await Users.all().count()
+    data = await Users.get_or_none(tg_username=username)
+
+    if data:
+        data.created_at = data.created_at.strftime("%Y-%m-%d %H:%M:%S")
+        data.updated_at = data.updated_at.strftime("%Y-%m-%d %H:%M:%S")
+
+    return templates.TemplateResponse(
+        "users.html", {"request": request, "data": data, "counter": counter}
+    )
 
 
 # Fake Route
-@app.get("/wp-admin")
-@app.get("/wp-login")
-@app.get("/admin")
+@app.get("/wp-admin", include_in_schema=False)
+@app.get("/wp-login", include_in_schema=False)
+@app.get("/admin", include_in_schema=False)
 async def fake_route(request: Request):
     return templates.TemplateResponse("fakeroute.html", {"request": request})
