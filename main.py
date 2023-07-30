0b8cdc5677e5e0c5a6b367a49f47866563d88b07
ByteCntrl <bytecntrl@gmail.com>
2023-07-30 15:11:39 +0200

    Update tools endpoint

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from config import Config, Session
-from core.api import auth, blacklist, statistics, test, users
+from core.api import auth, blacklist, statistics, test, tools, users
 from core.database import init_db
 
 # load .env file
@@ -31,4 +31,5 @@ app.include_router(auth.auth)
 app.include_router(blacklist.api_blacklist)
 app.include_router(statistics.api_statistics)
 app.include_router(test.api_test)
+app.include_router(tools.api_bot_service)
 app.include_router(users.api_users)
